bf635b8472106f41341f6b8b5a22e71ee65b3fc3
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-05-17 04:16:11 +0200

    fixed port and start script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require ('fs');
 const express = require('express');
 const hbs = require ('hbs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('view engine', 'hbs');
@@ -44,6 +44,6 @@ app.get('/bad', (req,res)=>{
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('Server is running on port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`);
 });
